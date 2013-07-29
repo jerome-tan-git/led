32a42c140a281c44725a379b754e4038f7351774
jetan <jetan@SI-JETAN02.corp.valueclick.com>
2013-07-29 13:47:51 +0800

    Test freemarker with Struts2 successfully

---
+++ ledweb/WebContent/WEB-INF/fm.ftl
@@ -0,0 +1,15 @@
+<#assign s=JspTaglibs["/WEB-INF/struts-tags.tld"] />
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<@s.form action="Action1.action">
+<@s.textfield name="username"></@s.textfield>
+<@s.textfield name="password"></@s.textfield>
+<@s.submit value="submit"/>
+</@s.form>
+<br/>name:${username}
+</body>
+</html>
---
+++ ledweb/src/ledweb/Action1.java
@@ -0,0 +1,25 @@
+package ledweb;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Action1 extends ActionSupport {
+	private String username = "111111";
+
+	public String getUsername() {
+		return username;
+	}
+
+	@Override
+	public String execute() {	
+		if(getUsername().equals("1"))
+		{
+			this.username = "a";
+		}
+		return SUCCESS;
+	}
+
+	public void setUsername(String username) {
+
+		this.username = username;
+	}
+}
