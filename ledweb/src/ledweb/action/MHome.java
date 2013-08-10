a84607ba5262849113fc69072c853ed867557c94
jerome-tan-git <tansheng_163@163.com>
2013-08-11 00:12:43 +0800

    doing mobile

---
+++ ledweb/src/ledweb/action/MHome.java
@@ -0,0 +1,11 @@
+package ledweb.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class MHome extends ActionSupport{
+	@Override
+	public String execute() {
+		
+		return SUCCESS;
+	}
+}
