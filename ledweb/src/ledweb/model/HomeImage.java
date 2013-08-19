b32ff6a83c722364b37329e8fa20ad9778681daf
jetan <jetan@SI-JETAN02.corp.valueclick.com>
2013-08-19 10:59:59 +0800

    Add home image table

---
+++ ledweb/src/ledweb/model/HomeImage.java
@@ -0,0 +1,14 @@
+package ledweb.model;
+
+public class HomeImage {
+	private String imageURL;
+
+	public String getImageURL() {
+		return imageURL;
+	}
+
+	public void setImageURL(String imageURL) {
+		this.imageURL = imageURL;
+	}
+
+}
